800f8ad958f92a03a4a74941211d3b65bfbbe4b0
Joachim Persson <joachim.persson88@gmail.com>
2023-09-17 17:30:36 +0200

    Har lagt till testmetod.

--- src/UserTest.java
+++ src/UserTest.java
@@ -10,4 +10,14 @@ public class UserTest {
         assertEquals("username", user.getUsername());
         assertEquals("password", user.getPassword());
     }
+    @Test
+    public void testEqualsMethod() {
+        User user1 = new User("username", "password1");
+        User user2 = new User("username", "password2");
+        User user3 = new User("username2", "password1");
+
+        assertTrue(user1.equals(user2));
+        assertFalse(user1.equals(user3));
+    }
+
 }
