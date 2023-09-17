34cd16e33df6bcc966d566ca82fa4978ba70b945
Joachim Persson <joachim.persson88@gmail.com>
2023-09-17 19:01:06 +0200

    Har skapat felhanteringsmetod.

--- src/AuthenticationServiceTest.java
+++ src/AuthenticationServiceTest.java
@@ -5,6 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
+    @Test
+    public void testAuthenticationUserNotFound() {
+        UserRepository mockRepo = Mockito.mock(UserRepository.class);
+        when(mockRepo.findUserByUsername("username")).thenReturn(null);
+
+        AuthenticationService service = new AuthenticationService(mockRepo);
+        assertThrows(AuthenticationException.class, () -> service.authenticate("username", "password"));
+    }
+
+    @Test
+    public void testAuthenticationWrongPassword() {
+        UserRepository mockRepo = Mockito.mock(UserRepository.class);
+        when(mockRepo.findUserByUsername("username")).thenReturn(new User("username", "correctPassword"));
+        AuthenticationService service = new AuthenticationService(mockRepo);
+        assertThrows(AuthenticationException.class, () -> service.authenticate("username", "wrongPassword"));
+    }
 
     @Test
     public void testAuthenticationSuccess() {
